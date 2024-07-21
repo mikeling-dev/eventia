e679846b98176c169d223b70580e5aead131c1a2
Mike Ling <mikeling.dev@gmail.com>
2024-07-21 22:14:59 +0800

    make auth form functional

--- src/core/components/mainAuth/index.tsx
+++ src/core/components/mainAuth/index.tsx
@@ -14,8 +14,25 @@ import {
   Stack,
 } from "@mantine/core";
 import { GoogleButton, TwitterButton } from "./SocialButtons";
+import { useMutation } from "@blitzjs/rpc";
+import login from "@/features/auth/mutations/login";
+import { FORM_ERROR } from "../Form";
+import { AuthenticationError, PromiseReturnType } from "blitz";
+import signup from "@/features/auth/mutations/signup";
+import { useEffect } from "react";
+
+type LoginFormProps = {
+  onSuccess?: (user: PromiseReturnType<typeof login>) => void;
+};
+
+type SignupFormProps = {
+  onSuccess?: () => void;
+};
 
 export function AuthenticationForm(props: PaperProps) {
+  const [loginMutation] = useMutation(login);
+  const [signupMutation] = useMutation(signup);
+
   const [type, toggle] = useToggle(["login", "register"]);
   const form = useForm({
     initialValues: {
@@ -24,12 +41,53 @@ export function AuthenticationForm(props: PaperProps) {
       password: "",
       terms: true,
     },
-
     validate: {
       email: (val) => (/^\S+@\S+$/.test(val) ? null : "Invalid email"),
       password: (val) => (val.length <= 6 ? "Password should include at least 6 characters" : null),
     },
   });
+
+  const onLogin = async (values) => {
+    try {
+      const user = await loginMutation(values);
+      // props.onSuccess?.(user);
+    } catch (error: any) {
+      if (error instanceof AuthenticationError) {
+        return { [FORM_ERROR]: "Sorry, those credentials are invalid" };
+      } else {
+        return {
+          [FORM_ERROR]:
+            "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+        };
+      }
+    }
+  };
+
+  const onSignup = async (values) => {
+    try {
+      await signupMutation(values);
+      // props.onSuccess?.();
+    } catch (error: any) {
+      if (error.code === "P2002" && error.meta?.target?.includes("email")) {
+        // This error comes from Prisma
+        return { email: "This email is already being used" };
+      } else {
+        return { [FORM_ERROR]: error.toString() };
+      }
+    }
+  };
+
+  const onSubmit = (values) => {
+    if (type === "login") {
+      onLogin(values);
+    } else {
+      onSignup(values);
+    }
+  };
+
+  useEffect(() => {
+    form.reset();
+  }, [type]);
 
   return (
     <Paper radius="md" p="xl" withBorder {...props}>
@@ -44,14 +102,13 @@ export function AuthenticationForm(props: PaperProps) {
 
       <Divider label="Or continue with email" labelPosition="center" my="lg" />
 
-      <form onSubmit={form.onSubmit(() => {})}>
+      <form onSubmit={form.onSubmit(onSubmit)}>
         <Stack>
           {type === "register" && (
             <TextInput
               label="Name"
               placeholder="Your name"
-              value={form.values.name}
-              onChange={(event) => form.setFieldValue("name", event.currentTarget.value)}
+              {...form.getInputProps("name")}
               radius="md"
             />
           )}
@@ -60,9 +117,7 @@ export function AuthenticationForm(props: PaperProps) {
             required
             label="Email"
             placeholder="hello@mantine.dev"
-            value={form.values.email}
-            onChange={(event) => form.setFieldValue("email", event.currentTarget.value)}
-            error={form.errors.email && "Invalid email"}
+            {...form.getInputProps("email")}
             radius="md"
           />
 
@@ -70,9 +125,7 @@ export function AuthenticationForm(props: PaperProps) {
             required
             label="Password"
             placeholder="Your password"
-            value={form.values.password}
-            onChange={(event) => form.setFieldValue("password", event.currentTarget.value)}
-            error={form.errors.password && "Password should include at least 6 characters"}
+            {...form.getInputProps("password")}
             radius="md"
           />
 
